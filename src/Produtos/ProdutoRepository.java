94dbdd6e5ad3fdc72f60974aca852d3eb1aaf73e
aline <aline.wornath@gmail.com>
2025-02-22 17:41:46 -0300

    Cria uma classe Produto e um ProdutoRepository

---
+++ src/Produtos/ProdutoRepository.java
@@ -0,0 +1,34 @@
+package Produtos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProdutoRepository {
+    public static List<Produto> listaProdutos = new ArrayList<Produto>();
+
+    public static void cadastrarProduto(Produto produto) {
+        listaProdutos.add(produto);
+        System.out.println("Produto " + produto.getNome() +" cadastrado com sucesso!");
+    }
+
+    public static void listarProdutos() {
+        for (Produto produto : listaProdutos) {
+            System.out.println(produto);
+        }
+    }
+
+    public static void atualizarProduto(int id, String novoNome, String novaCategoria, double novoValorDeVenda, double novoDesconto) {
+        for (Produto produto : listaProdutos) {
+            if(produto.getId() == id) {
+                produto.setNome(novoNome);
+                produto.setCategoria(novaCategoria);
+                produto.setValorDeVenda(novoValorDeVenda);
+                produto.setDesconto(novoDesconto);
+                System.out.println("Produto " + produto.getId() + " atualizado com sucesso.");
+                return;
+            }
+        }
+        System.out.println("Produto não encontrado.");
+    }
+
+}
