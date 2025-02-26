35500eecb99c80981ffe5e13ed440f55e69c4df6
aline <aline.wornath@gmail.com>
2025-02-26 12:47:07 -0300

    Ajuste em função duplicada de ProdutoRepository e ProdutoService

--- src/Produtos/ProdutoService.java
+++ src/Produtos/ProdutoService.java
@@ -1,26 +1,11 @@
 package Produtos;
 
-import java.util.List;
-
-import static Produtos.ProdutoRepository.listaProdutos;
-
 public class ProdutoService {
 
-    public Produto buscarProduto(int id, List<Produto> listaProdutos) {
-        if (listaProdutos.isEmpty()) {
-            System.out.println("Nenhum produto cadastrado");
-        } else {
-            for (Produto produto : listaProdutos) {
-                if (produto.getId() == id) {
-                    return produto;
-                }
-            }
-        }
-        return null;
-    }
+    private ProdutoRepository repository = new ProdutoRepository();
 
     public void atualizarProduto(int id, String novoNome, String novaCategoria, double novoValorDeVenda, double novoDesconto) {
-        Produto produto = ProdutoRepository.buscarProduto(id);
+        Produto produto = repository.buscarProduto(id);
             if(produto != null) {
                 produto.setNome(novoNome);
                 produto.setCategoria(novaCategoria);
